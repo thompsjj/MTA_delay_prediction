e142b385892f6bb56bf66edfb3c4a70329097dca
gSchool Student <gSchool@Datas-Mac-mini-29.local>
2015-03-02 17:16:37 -0800

    sql boilerplate 1

---
+++ sql_interface.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 24 08:02:02 2015
+
+@author: Jared
+"""
+
+import psycopg2
+
+def connect_to_local_db(name, user, password='user'):
+    try:
+        conn = psycopg2.connect(database=name, user=user, host='localhost', password=password)
+        conn.autocommit=True
+    except:
+        print "Unable to connect to the database"
+    
+
+    cursor = conn.cursor()
+    cursor.execute("""SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'""")
+    for table in cursor.fetchall():
+        print(table)
+
+    return cursor, conn
+
+def table_exists(cur, table_str):
+    exists = False
+    if cur.closed==True:
+        print "cursor is closed."
+        return False
+    try:
+        cur.execute("SELECT EXISTS(SELECT relname FROM pg_class WHERE relname='" + table_str + "')")
+        exists = cur.fetchone()[0]
+    except psycopg2.Error as e:
+        print e
+    return exists
+    
+def drop_table(cur, table_name):
+    if table_exists(cur, table_name):
+        if cur.closed==False:            
+            cur.execute("DROP TABLE %s" % (table_name))
+        else:
+            print 'cursor is closed.'
---
+++ sql_schema.py
@@ -0,0 +1,40 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 24 08:02:02 2015
+
+@author: Jared J. Thompson
+"""
+
+
+def create_MTA_ETD_schema(cur, table_name):
+    if table_exists(cur, table_name):
+        print "table %s exists already." % table_name
+        return None
+    else:
+        if cur.closed==False:
+            cur.execute("CREATE TABLE %s( station_id varchar(20), destination_id varchar(20),\
+            etd_sample real, delay real, sample_time timestamp);" % table_name)
+        else:
+            print 'cursor is closed.'
+
+def create_MTA_schedule_schema(cur, table_name):
+    if table_exists(cur, table_name):
+        print "table %s exists already." % table_name
+        return None
+    else:
+        if cur.closed==False:
+            cur.execute("CREATE TABLE %s( trip_name varchar(30), trip_type varchar(30),\
+            station_id varchar(30), destination_id varchar(30),\
+            line_name_id varchar(40), departure_time interval);" % table_name)
+        else:
+            print 'cursor is closed.'
+            
+def update_MTA_schedule_schema(cur, table_name, input_v):
+    if table_exists(cur, table_name):
+        if cur.closed==False:            
+            cur.execute("INSERT INTO %s( trip_name, trip_type, station_id, destination_id,\
+            line_name_id, departure_time) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" % (table_name, input_v['trip_name'], input_v['trip_type'],\
+                        input_v['station_id'],input_v['destination_id'],input_v['line_name_id'],input_v['departure_time']))
+        else:
+            print 'cursor is closed.'
